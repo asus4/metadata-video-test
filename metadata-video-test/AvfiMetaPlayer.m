a939a29cebaac35b25a539f6d5d649745e0313e8
Koki Ibukuro <koki.ibukuro@gmail.com>
2022-07-12 14:47:11 +0200

    Implement loading metadata track from mp4

--- metadata-video-test/AvfiMetaPlayer.m
+++ metadata-video-test/AvfiMetaPlayer.m
@@ -1,27 +1,108 @@
-//
-//  AvfiMetaPlayer.m
-//  metadata-video-test
-//
-//  Created by Koki Ibukuro on 07.07.22.
-//
+#import <AVFoundation/AVFoundation.h>
 
-#import <Foundation/Foundation.h>
+@interface RawMetadata: NSObject
+@property (nonatomic, assign) CMTime time;
+@property (nonatomic, retain) NSData* data;
+@end
 
-static bool _isPlaying = true;
+@implementation RawMetadata
+@end
 
-extern void Avfi_LoadPlayer(const char* filePath) {
-    NSLog(@"Avfi_LoadPlayer");
+
+static AVAssetReader* _reader;
+static AVAssetReaderTrackOutput* _readerMetadataOutput;
+static AVAssetReaderOutputMetadataAdaptor* _metadataAdaptor;
+
+static NSMutableArray<RawMetadata*>* _metadataBuffer = nil;
+#define kMETADATA_ID_RAW @"mdta/com.github.asus4.avfi.raw"
+
+
+extern bool Avfi_LoadPlayer(const char* filePath) {
+    // Load AVAsset from file path
+    NSURL* url = [NSURL fileURLWithPath:[NSString stringWithUTF8String:filePath]];
+    AVAsset* asset = [AVAsset assetWithURL:url];
+
+    NSLog(@"Avfi_LoadPlayer: url:%@, asset:%@", url, asset);
+
+    // Ensure the asset has at leaset one metadata track.
+    NSArray* tracks = [asset tracksWithMediaType:AVMediaTypeMetadata];
+    if (tracks.count == 0) {
+        NSLog(@"No metadata track found in the video file: %@.", url);
+        return false;
+    }
+    
+    NSError* error;
+    _reader = [AVAssetReader assetReaderWithAsset:asset error:&error];
+    if (error) {
+        NSLog(@"Failed to initialize AVAssetReader (%@)", error);
+        return false;
+    }
+
+    // At the moment, we only support one metadata track.
+    NSLog(@"Found %i tracks", (int)tracks.count);
+    AVAssetTrack* track = tracks[0];
+
+    // Create a reader output for the metadata track.
+    _readerMetadataOutput = [AVAssetReaderTrackOutput assetReaderTrackOutputWithTrack:track outputSettings:nil];
+    [_reader addOutput:_readerMetadataOutput];
+
+    // Create a metadata adaptor for the reader output
+    _metadataAdaptor = [AVAssetReaderOutputMetadataAdaptor assetReaderOutputMetadataAdaptorWithAssetReaderTrackOutput:_readerMetadataOutput];
+    
+    _metadataBuffer = [[NSMutableArray alloc] init];
+    [_reader startReading];
+
+    // Read all metadata from the adaptor 
+    AVTimedMetadataGroup* group;
+    int i = 0;
+    while(true) {
+        group = [_metadataAdaptor nextTimedMetadataGroup];
+        if(group == nil) {
+            break;
+        }
+     
+        CMTimeRange timeRange = group.timeRange;
+        for(AVMetadataItem* item in group.items) {
+            if ([item.identifier isEqualToString:kMETADATA_ID_RAW]) {
+                RawMetadata* rawMetadata = [[RawMetadata alloc] init];
+                rawMetadata.time = timeRange.start;
+                rawMetadata.data = (NSData*) item.value;
+
+                [_metadataBuffer addObject:rawMetadata];
+            }
+        }
+
+        NSLog(@"%i Found metadata group: %@", i, group);
+
+        i++;
+    }
+
+    [_reader cancelReading];
+    return true;
 }
 
-extern void Avfi_StartPlaying(void) {
-    NSLog(@"Avfi_StartPlaying");
+extern uint32_t Avfi_GetBufferSize(void)
+{
+    // Find max byte size of all metadata
+    uint32_t maxSize = 0;
+    for(RawMetadata* rawMetadata in _metadataBuffer) {
+        maxSize = MAX(maxSize, (uint32_t)rawMetadata.data.length);
+    }
+    return maxSize;
 }
 
-extern void Avfi_StopPlaying(void) {
-    NSLog(@"Avfi_StopPlaying");
-}
+extern uint32_t Avfi_PeekMetadata(double time, void* data)
+{
+    if(_metadataBuffer == nil) {
+        return 0;
+    }
 
-extern bool Avfi_IsPlaying(void) {
-    _isPlaying = !_isPlaying;
-    return _isPlaying;
+    for(RawMetadata* rawMetadata in _metadataBuffer) {
+        if(CMTimeCompare(rawMetadata.time, CMTimeMakeWithSeconds(time, 240)) == 0) {
+            uint32_t length = (uint32_t)rawMetadata.data.length;
+            memcpy(data, rawMetadata.data.bytes, rawMetadata.data.length);
+            return length;
+        }
+    }
+    return 0;
 }
--- metadata-video-test/ViewController.m
+++ metadata-video-test/ViewController.m
@@ -6,33 +6,57 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-
-
-extern void Avfi_LoadPlayer(const char* filePath);
-extern void Avfi_StartPlaying(void);
-extern void Avfi_StopPlaying(void);
-extern bool Avfi_IsPlaying(void);
+extern bool Avfi_LoadPlayer(const char* filePath);
+extern uint32_t Avfi_GetBufferSize(void);
+extern uint32_t Avfi_PeekMetadata(double time, void* data);
 
 @interface ViewController ()
-
+{
+    void* _metadataBuffer;
+    double _videoDuration;
+}
+@property (weak, nonatomic) IBOutlet UILabel *infoLabel;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    Avfi_LoadPlayer("aaaaaaa");
+    // Load video file from asset
+    NSString* filePath = [[NSBundle mainBundle] pathForResource:@"sample" ofType:@"mp4"];
+
+    // Get video duration 
+    AVURLAsset* asset = [AVURLAsset assetWithURL:[NSURL fileURLWithPath:filePath]];
+    CMTime duration = asset.duration;
+    _videoDuration = CMTimeGetSeconds(duration);
+    NSLog(@"Video duration: %f", _videoDuration);
+
+    Avfi_LoadPlayer([filePath UTF8String]);
+
+    uint32_t bufferSize = Avfi_GetBufferSize();
+    NSLog(@"bufferSize: %i", bufferSize);
+
+    // Create a buffer to store metadata
+    _metadataBuffer = malloc(bufferSize);
 }
 
+- (void)viewDidDisappear:(BOOL)animated {
+    free(_metadataBuffer);
+    _metadataBuffer = NULL;
+}
 
-- (IBAction)onButtonTap:(id)sender {
-    if(Avfi_IsPlaying()) {
-        Avfi_StopPlaying();
-    } else {
-        Avfi_StartPlaying();
-    }
+- (IBAction)onSliderChange:(UISlider *)sender {
+    NSLog(@"onSliderChange: %f", sender.value);
+
+    double time = sender.value * _videoDuration;
+    // Get metadata from the buffer
+    Avfi_PeekMetadata(time, _metadataBuffer);
+
+    // Convert metadata to double array
+    double* data = (double*)_metadataBuffer;
+    self.infoLabel.text = [NSString stringWithFormat:@"%f %f", data[0], data[1]];
 }
 
 @end
